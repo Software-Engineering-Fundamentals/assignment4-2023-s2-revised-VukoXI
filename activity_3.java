e177c185ac80ae68afd6ca63c3389fdb7903e72f
Stefan Vukomanovic <vukos016@gmail.com>
2023-10-16 20:08:57 +1100

    updated and completed task 3

--- activity_3.java
+++ activity_3.java
@@ -3,9 +3,7 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Department in an organisation
- */
+
 public class Department {
     
     private String name;
@@ -14,12 +12,12 @@ public class Department {
     
     private ArrayList<String> temp1 = new ArrayList<String>();
 
-    private ArrayList<int> employee = new ArrayList<int>();
-
+    //private ArrayList<int> employee = new ArrayList<int>(); // cannot use generics with primative eg int should be integer in the array list 
+    private ArrayList<Integer> employee = new ArrayList<Integer>();
     
     private int ID2;
 
-    private String name;
+    //private String name; //this is a duplicate therefore needs to be removed
 
     public String getID() {
         return ID2;
@@ -29,34 +27,26 @@ public class Department {
         this.ID2 = ID;
     }
 
-    
-    public boolean checkEmployee(int eID) {
-
-	int p=0;
-	for (int i = 0; i < this.employee.size(); i++){
-check=this.employee.get(i);
-            if (check==eID) {
-                p=1;
-            }
+    //refractor
+ public boolean checkEmployee(int eID){
+    //better readability and maintainability
+    // Use enhanced for loop for better readability
+    for (int employeeID : this.employee) {
+        if (employeeID == eID) {
+            System.out.println("Employee exists");
+            return true;
         }
-
-        if (p==1)
-{
-System.out.println("Employee exists");
-return true;
-}
-else{
-System.out.println("Employee doesn't exists");
-return false;
-	    }
     }
+    // If the loop completes without finding a match, indicate that the employee doesn't exist
+    // Moved the "Employee doesn't exist" message outside the loop for clarity
+    System.out.println("Employee doesn't exist");
+    return false;
+}
 
     
 
-    public void assignNew(string temp2, int ID) {
+    public void assignNew(String temp2, int ID) { //minor but capitalisation for string to String 
         this.temp1.add(temp2);
-	  this.ID1.add(ID);
+        this.ID1.add(String.valueOf(ID)); // Corrected the parameter type and conversion
     }
-
-
 }
