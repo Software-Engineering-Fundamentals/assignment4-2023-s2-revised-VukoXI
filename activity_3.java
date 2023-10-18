4e92ed1dbd6252bdf89cb5e31abe8d9a3c2251dc
Stefan Vukomanovic <vukos016@gmail.com>
2023-10-18 14:04:11 +1100

    reverting changes

--- activity_3.java
+++ activity_3.java
@@ -3,21 +3,23 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * Department in an organisation
+ */
 public class Department {
-    
+
     private String name;
-    
+
     private ArrayList<String> ID1 = new ArrayList<String>();
-    
+
     private ArrayList<String> temp1 = new ArrayList<String>();
 
-    //private ArrayList<int> employee = new ArrayList<int>(); // cannot use generics with primative eg int should be integer in the array list 
-    private ArrayList<Integer> employee = new ArrayList<Integer>();
-    
+    private ArrayList<int> employee = new ArrayList<int>();
+
+
     private int ID2;
 
-    //private String name; //this is a duplicate therefore needs to be removed
+    private String name;
 
     public String getID() {
         return ID2;
@@ -27,26 +29,34 @@ public class Department {
         this.ID2 = ID;
     }
 
-    //refractor
- public boolean checkEmployee(int eID){
-    //better readability and maintainability
-    // Use enhanced for loop for better readability
-    for (int employeeID : this.employee) {
-        if (employeeID == eID) {
-            System.out.println("Employee exists");
-            return true;
+
+    public boolean checkEmployee(int eID) {
+
+	int p=0;
+	for (int i = 0; i < this.employee.size(); i++){
+check=this.employee.get(i);
+            if (check==eID) {
+                p=1;
+            }
         }
-    }
-    // If the loop completes without finding a match, indicate that the employee doesn't exist
-    // Moved the "Employee doesn't exist" message outside the loop for clarity
-    System.out.println("Employee doesn't exist");
-    return false;
+
+        if (p==1)
+{
+System.out.println("Employee exists");
+return true;
 }
-
-    
-
-    public void assignNew(String temp2, int ID) { //minor but capitalisation for string to String 
-        this.temp1.add(temp2);
-        this.ID1.add(String.valueOf(ID)); // Corrected the parameter type and conversion
+else{
+System.out.println("Employee doesn't exists");
+return false;
+	    }
     }
+
+
+
+    public void assignNew(string temp2, int ID) {
+        this.temp1.add(temp2);
+	  this.ID1.add(ID);
+    }
+
+
 }
